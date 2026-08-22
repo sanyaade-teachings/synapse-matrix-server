3b77e463bffb0fbc065632c097c5e6a3848645e3
sanyaade-teachings <sanyaade-teachings@users.noreply.github.com>
2026-08-22 13:47:59 +0000

    deploy: 3debaff0f86c1f34d5c5970bd5a10bea8a67b266

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a3182bd5.js',
+                path_to_root + 'searchindex-cf21f0fd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
